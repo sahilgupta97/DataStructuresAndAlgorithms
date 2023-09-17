897116875daea2338453f3bc258d382acd6de2b3
sahilgupta97 <guptasahilonweb2@gmail.com>
2023-09-17 22:15:02 +0530

    added implementation of blocking unbounded & improved bounded queue with two locks

--- src/main/java/dataStructureDesign/Queues/BlockingUnboundedQueue.java
+++ src/main/java/dataStructureDesign/Queues/BlockingUnboundedQueue.java
@@ -6,41 +6,33 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * This queue does wait as it has defined bounds for adding the items in it.
+ * This queue does not wait as it has no bounds for adding the items in it
  */
-public class BlockingBoundedQueue<T> {
+public class BlockingUnboundedQueue<T> {
 
   private final Queue<T> queue;
-  private final int capacity;
   private final ReentrantLock offerLock;
   private final ReentrantLock pollLock;
-  private final Condition notFullCondition;
   private final Condition notEmptyCondition;
 
-  public BlockingBoundedQueue(int capacity) {
+  public BlockingUnboundedQueue() {
     this.queue = new LinkedList<>();
-    this.capacity = capacity;
     this.offerLock = new ReentrantLock();
     this.pollLock = new ReentrantLock();
     this.notEmptyCondition = this.pollLock.newCondition();
-    this.notFullCondition = this.offerLock.newCondition();
   }
 
-  public void offer(T item) throws InterruptedException {
+  public boolean offer(T item) {
     offerLock.lock();
 
     try {
-      while (queue.size() == capacity) {
-        // wait till queue becomes (not full) => till there is some space
-        notFullCondition.await();
-      }
       queue.offer(item);
-
-      // signal all the threads that now the queue is not empty i.e. chance create for poll to take place
       notEmptyCondition.signalAll();
     } finally {
       offerLock.unlock();
     }
+
+    return true;
   }
 
   public T poll() throws InterruptedException {
@@ -48,13 +40,10 @@ public class BlockingBoundedQueue<T> {
 
     try {
       while (queue.isEmpty()) {
-        // wait till queue becomes (not empty) => it at least has one item
         notEmptyCondition.await();
       }
       return queue.poll();
     } finally {
-      // signal all threads that queue is now not full i.e. space created for offering to it
-      notFullCondition.signalAll();
       pollLock.unlock();
     }
   }
